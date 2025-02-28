c3e004f50c074a873b9ed087e858e9cb7f3523b5
Muhammad Bilal <mbilal254.m@gmail.com>
2025-03-01 04:17:23 +0500

    create http server

--- sheriyans_tut/server.js
+++ sheriyans_tut/server.js
@@ -1,7 +1,19 @@
-import catMe from "cat-me"
+import http from "http"
 
-console.log(catMe());
-console.log(catMe());
-console.log(catMe());
-console.log(catMe());
-console.log(catMe());
+const server = http.createServer((req, res) => {
+    console.log(req.url);
+    if (req.url === "/") {
+        res.end("Hello")
+    }
+    else if (req.url === "/about") {
+        res.end("This is about")
+
+    }
+
+})
+
+// run on port 3000
+server.listen(3000, () => {
+    console.log("The app is running on port: 3000");
+
+})
